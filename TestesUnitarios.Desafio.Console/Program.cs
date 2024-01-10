b47f352506a8644cb5595f90ce87bdef42a08990
Igor Mesquita <igormesquitab25@gmail.com>
2024-01-10 13:51:56 -0300

    Desafio concluído!

--- TestesUnitarios.Desafio.Console/Program.cs
+++ TestesUnitarios.Desafio.Console/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TestesUnitarios.Desafio.Console.Services;
+
+ValidacoesLista listas = new ValidacoesLista();
+List<int> lista = new List<int>{5, -1, -8, 9};
+Console.WriteLine(listas.RetornarMenorNumeroLista(lista));
+
+
